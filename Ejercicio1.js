b8f1f8e76672030d6476b08e38ed8a676b408c2e
Brandon-Nunez <65833012+Brandon-Nunez@users.noreply.github.com>
2020-08-14 20:26:06 -0500

    Create Ejercicio1.js

---
+++ Ejercicio1.js
@@ -0,0 +1,7 @@
+var base = 15;
+var altura = 10;
+var area = base * altura;
+function area_rectangulo(base, altura) {
+  console.log('El Area del rectangulo es ' + area );
+}
+area_rectangulo(area);
